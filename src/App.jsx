3804c2774df6cb149ae6340c737d4a90ffe7b4a7
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2024-10-26 23:16:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
+function App() {
+  return (
+  <div className="bg-[#f3f4b911 h-auto w-full overflow-hidden">
+  <Navbar />
+  <Home />
+  <About />
+  <Experience />
+  <Projects />
+  <Footer />
+</div>
+  );
+}
+
+export default App;
